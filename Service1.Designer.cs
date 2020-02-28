0c0755ededfd8e9601dc397d17e1c5c541cd88a7
Михаил <mbalychev@gmail.com>
2020-02-28 16:35:59 +1000

    Добавьте файлы проекта.

---
+++ Service1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace BmmService
+{
+    partial class Service
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).BeginInit();
+            // 
+            // Service
+            // 
+            this.CanStop = false;
+            this.ServiceName = "BmmService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog;
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmmService
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+            eventLog = new EventLog();
+            if (!EventLog.SourceExists("MbbSource"))
+            {
+                EventLog.CreateEventSource(
+                "MbbSource", "MbbLog");
+            }
+            eventLog.Source = "MbbSource";
+            eventLog.Log = "MbbLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Start service");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Stop service");
+        }
+    }
+}
